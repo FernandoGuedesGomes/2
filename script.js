19efad81739ce76419a0d1bef2e5daeab28ccad9
FernandoGuedesGomes <112212733+FernandoGuedesGomes@users.noreply.github.com>
2022-08-31 12:34:04 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-alert("Seja bem vindo ao jogo Acerte o Número! Nesse jogo você deve acertar um numero de 1 a 10 que o programa vai gerar automaticamente. Boa sorte!");
+alert("Seja bem vindo ao jogo Acerte o Número! Nesse jogo você deve acertar um numero aleatório de 1 a 100. Boa sorte!");
 
+let aleatorio = Number(Math.floor(Math.random() * 100) + 1);
+console.log(aleatorio);
+
+let textH1 = document.getElementById("h1");
 
 do {
-    var aleatorio = Math.floor(Math.random() * 10) + 1;
-    console.log(aleatorio);
-    var numero = prompt("Escreva um numero de 1 a 10:");
+    var numero = Number(prompt("Digite um numero de 1 a 100:"));
 
     if (aleatorio > numero) {
         alert("Seu chute foi menor, tente novamente!");
@@ -12,14 +14,11 @@ do {
     else if (aleatorio < numero) {
         alert("Seu chute foi maior, tente novamente!");
     }
-    else if (aleatorio == numero) {
-        alert("Parabens! Você acertou o numero " + aleatorio + ".");
-        break;
-    }
+    else if (aleatorio === numero){}
     else {
         alert("houve um erro. Tente novamente.");
     }
-
-
 } while (aleatorio !== numero);
+
+textH1.innerHTML = `Parabens voce acertou!! O número sorteado foi ${aleatorio}.`;
 
